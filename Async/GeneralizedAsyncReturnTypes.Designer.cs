cccd2dc240941ed1eefbfd7b6cf3a05c9b01a06d
Tandi Sunarto <tandi.sunarto@hotmail.com>
2017-10-03 21:04:41 -0400

    ValueTask

---
+++ Async/GeneralizedAsyncReturnTypes.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Async
+{
+    partial class GeneralizedAsyncReturnTypes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnTestAsync = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(449, 335);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtOutput);
+            this.tabPage1.Controls.Add(this.lblTimer);
+            this.tabPage1.Controls.Add(this.btnTestAsync);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(441, 309);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Generalized Return Type";
+            this.tabPage1.Enter += new System.EventHandler(this.tabPage1_Enter);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(113, 23);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(225, 20);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(25, 61);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 13);
+            this.lblTimer.TabIndex = 1;
+            // 
+            // btnTestAsync
+            // 
+            this.btnTestAsync.Location = new System.Drawing.Point(17, 21);
+            this.btnTestAsync.Name = "btnTestAsync";
+            this.btnTestAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnTestAsync.TabIndex = 0;
+            this.btnTestAsync.Text = "Test Async";
+            this.btnTestAsync.UseVisualStyleBackColor = true;
+            this.btnTestAsync.Click += new System.EventHandler(this.btnTestAsync_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(441, 309);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // GeneralizedAsyncReturnTypes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 359);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "GeneralizedAsyncReturnTypes";
+            this.Text = "Async Form";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnTestAsync;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Async/GeneralizedAsyncReturnTypes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public partial class GeneralizedAsyncReturnTypes : Form
+    {
+        double timerTTL = 10.0D;
+        private DateTime timeTolLive;
+        private int cacheValue;
+
+        public GeneralizedAsyncReturnTypes()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Enter(object sender, EventArgs e)
+        {
+            lblTimer.Text = $"Timer TTL {timerTTL} sec (Stopped)";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timerTTL == 0)
+                timerTTL = 5;
+            else
+                timerTTL -= 1;
+            lblTimer.Text = $"Timer TTL {timerTTL} sec (Running)";
+        }
+
+        public async Task<int> GetValue()
+        {
+            await Task.Delay(2000);
+
+            Random r = new Random();
+            cacheValue = r.Next();
+            timeTolLive = DateTime.Now.AddSeconds(timerTTL);
+            timer1.Start();
+            return cacheValue;
+        }
+
+        public ValueTask<int> LoadReadCache(out bool cached)
+        {
+            if (timeTolLive < DateTime.Now)
+            {
+                cached = false;
+                return new ValueTask<int>(GetValue());
+            }
+            else
+            {
+                cached = true;
+                return new ValueTask<int>(cacheValue);
+            }
+        }
+
+
+        private async void btnTestAsync_Click(object sender, EventArgs e)
+        {
+            int value = await LoadReadCache(out bool isCached);
+
+            if (isCached)
+            {
+                txtOutput.Text = $"Cached value {value} read";
+            }
+            else
+            {
+                txtOutput.Text = $"New value {value} read";
+            }
+        }
+    }
+}
