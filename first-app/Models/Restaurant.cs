6b667935d901a3e8cc2e0e4c2a814d1ef20940a2
tandisunarto <tandi.sunarto@hotmail.com>
2017-11-28 18:00:32 -0500

    final update on the dbcontext

--- first-app/Models/Restaurant.cs
+++ first-app/Models/Restaurant.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 public class Restaurant 
 {
     public int Id { get; set; }
-    [Display(Name="Restaurant Name")]
+    [Display(Name="Restaurant Name"), MaxLength(80)]
     public string Name { get; set; }
     public CuisineType Cuisine { get; set; }
 }
