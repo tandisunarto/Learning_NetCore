2b54148e93703c74f6b19ef96ccd741dda1ac57a
tandisunarto <tandi.sunarto@hotmail.com>
2019-04-23 17:29:04 -0400

    so far so good ...

---
+++ ODataWebAPI/appContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ODataWebAPI
+{
+    public partial class appContext : DbContext
+    {
+        public appContext()
+        {
+        }
+
+        public appContext(DbContextOptions<appContext> options)
+            : base(options)
+        {
+        }
+
+        // Unable to generate entity type for table 'Product'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("DataSource=app.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+        }
+    }
+}
